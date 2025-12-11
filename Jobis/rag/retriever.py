3659fd93d1f785d6052a26f7039fc345e54f3d5b
gyeoulnamu <wkoilz1028@gmail.com>
2025-12-11 19:16:12 +0900

    search_type 변경 및 정리

--- Jobis/rag/retriever.py
+++ Jobis/rag/retriever.py
@@ -1,6 +1,11 @@
-def get_retriever(vectorstore, k=5):
+def get_retriever(vectorstore, k=3):
     retriever = vectorstore.as_retriever(
-        search_type="similarity",
-        search_kwargs={"k": k} 
-    )
+            search_type="mmr",
+            search_kwargs={
+                "k": k,            # 최종적으로 가져올 문서 개수
+                "fetch_k": k * 2,  # 다양성 계산을 위해 후보로 먼저 가져올 문서 개수
+                "lambda_mult": 0.5 # 0 ~ 1 사이 (0.5는 다양성과 유사도 균형)
+            } 
+        )
+    
     return retriever
